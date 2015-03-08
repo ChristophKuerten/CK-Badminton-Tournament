b3f3a38dc1148d77116a6f079e7fcd39c72e514e
ChristophKuerten <christophkuerten@t-online.de>
2015-03-08 20:16:12 +0100

    Setlisting and First Round
    Calculates a random setlist and advise the first round of the tournament

--- js/bootstrap.tunier.js
+++ js/bootstrap.tunier.js
@@ -3,6 +3,10 @@ var playercount;
 var playerTableCode;
 var playerDataforTree = {player:[]};		//solution and reason: "value",  http://stackoverflow.com/questions/4538269/adding-removing-items-from-json-data-with-jquery
 var tournamentTreefirstroundcount;
+var randomNumber;
+var startedplayer = 0;
+var setzliste = [];
+var setzlisterLaenge = 1;
 function calculateProgress (valueProgressAdd){
 //	console.log("bin in der function "+ valueProgressAdd+ " "+ valueProgressOld);
 	valueProgressOld = $("#progressTunier").attr("aria-valuenow");
@@ -37,9 +41,9 @@ function fillSinglePlayerlist(name,vorname,verein){
 	tableClass = "";
 	//console.log(playerTableCode);
 	if (playercount%2 == 0){
-    	tableClass ="active"
-    };
-    playerTableCode ="<tr class='"+tableClass+"'><th scope='row'>"+playercount+"</th><td>"+name+"</td><td>"+vorname+"</td><td>"+verein+"</td></tr>"
+    	tableClass ="active";
+    }
+    playerTableCode ="<tr class='"+tableClass+"'><th scope='row'>"+playercount+"</th><td>"+name+"</td><td>"+vorname+"</td><td>"+verein+"</td></tr>";
     $("#playerTable tbody").append(playerTableCode);
 
 
@@ -54,62 +58,92 @@ function activaTab(tab){
 };
 
 function startTournament(){
-	startedplayer = (playerDataforTree.player.length());
-	var setzliste = [];
-	var setzlisterLaenge = setzliste.length;
+	var i = 0;
 	var z = 0;
-	while (i < startedplayer){
-		var player= Math.floor(Math.random() * (startedplayer - 0 + 1));
-		for (z < setzlisterLaenge){
-			if (setzliste[z] == player){
-				z = 9999;
-			}else{
-				z++;
-			}
-		};
-		if (z == 9999){
-			i++;
-		}else{
-			setzliste.push(player);
-			i++;
-		};
-		setzlisterLaenge= setzlisterLaenge +1;
-	};
+	while (setzlisterLaenge < startedplayer){
+		createsetlist(i,z);
+//		console.log(setzliste);
+		setzlisterLaenge = setzliste.length;
+	}
 	console.log(setzliste);
 	loopplayer = startedplayer + 1;
-	var i = 0;
 	tournamentTreefirstroundcount = Math.round(startedplayer/2);
 	if (playercount%2 == 0){
-		console.log("gerade SpielerAnzalh"+startedplayer);
+		console.log("gerade SpielerAnzalh "+startedplayer);
 		while (i < startedplayer){
-			var playerofarray = (playerDataforTree.player[i]);
+			var setlistplayerOne = setzliste[i] -1;
+			var setlistplayerTwo = setzliste[i+1] -1;
+		//	console.log (setlistplayerOne);
+			var playerofarrayOne = playerDataforTree.player[setlistplayerOne];
+			var playerofarrayTwo = playerDataforTree.player[setlistplayerTwo];
+		//	console.log (playerofarrayOne.name);
+			playerTableCode ="<p>"+playerofarrayOne.name+", "+playerofarrayOne.vorname+ " vs. "+playerofarrayTwo.name+", "+playerofarrayTwo.vorname+"</p>";
+			$("#tournamentMatches").append(playerTableCode);
 
 
 
-			console.log (playerofarray.name);
-			i++;
+
+	//		console.log (playerofarray.name);
+			i=i+2;
 		};
 	}else {
-		console.log("ungerade SpielerAnzalh"+startedplayer);
+		console.log("ungerade SpielerAnzalh "+startedplayer);
 		while (i < startedplayer){
+			var setlistplayer = setzliste[i];
+			var playerofarray = playerDataforTree.player[setlistplayer];
 			var playerofarray = (playerDataforTree.player[i]);
 
 
 
-			console.log (playerofarray.name);
+			//console.log (playerofarray.name);
 			i++;
 		};
 	};
-	console.log(tournamentTreefirstroundcount);
+//	console.log(tournamentTreefirstroundcount);
 	
 
 	
 	
-//	var matchplan = "<div id='game1'>"+ 
-//	$("#tournamentMatches").append(playerTableCode);
+
+};
+function createsetlist(i,z){
+	while (i < startedplayer){
+		randomNumber= Math.random();
+		var player = Math.round(randomNumber * (startedplayer-1)+1);
+	//	console.log("Spieler: "+player);
+		z = 0;
+	//	console.log("Z1: "+z);
+		while (z < startedplayer){
+			var zControl = setzliste[z];
+			var zControlTest = $.isNumeric(zControl);
+		//	console.log(zControlTest);
+		// 	console.log("while: "+ i +" for: "+z+" Player: "+player +" with zContorl: "+zControl);
+		//	console.log("Value in Array: "+ zControl + " = Next Value: " + player);
+		 	if (zControl == player){
+		// 		console.log("Die nummer gibt es schon: " + player);
+		 		z = startedplayer;
+
+		 	}else if (zControlTest == true){
+		 		z++;
+		 	}else{
+		 		console.log
+		 		setzliste[z] = player;
+		 		z = startedplayer
+		 	}
+		}
+
+		i++;
+	}
 };
 
 $( document ).ready(function() {
+// =============================== Test der Setzlisten Schleife ==========================================================
+
+
+
+// =============================== Test der Setzlisten Schleife ==========================================================
+
+
 	playercount = 0;
 //    console.log( "ready!" );
     $("#secondGeschlecht").prop("disabled", true);
@@ -204,6 +238,7 @@ $( document ).ready(function() {
     	if (playerold <= controlerPlayer) {
     		fillSinglePlayerlist(nameEinzel, vornameEinzel, vereinEinzel);
     	};
+    	startedplayer = startedplayer + 1;
     	
    		//console.log(nameEinzel +"  "+vornameEinzel+" "+vereinEinzel);
    	});
